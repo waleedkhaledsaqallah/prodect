a090b4d86687a5e9f70bf70d9fd3e02edaf8d8af
waleed saqallah <137045087+waleedkhaledsaqallah@users.noreply.github.com>
2023-08-28 22:34:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+
+/************ start goup***************/
+let btndown = document.querySelector("#goup");
+addEventListener("scroll" , function(){
+})
+
+btndown.onclick = function(){
+scrollTo(0 , 0)
+}
